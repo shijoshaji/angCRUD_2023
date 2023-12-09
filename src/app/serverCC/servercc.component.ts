6af697e87e8f832b54f39124372de4e3d60a4cf4
Shijo Shaji <jojovms@gmail.com>
2023-12-09 09:50:38 +0530

    created component manually

---
+++ src/app/serverCC/servercc.component.ts
@@ -0,0 +1,25 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-servercc',
+    // template:`<p>Two types</p>`,
+    templateUrl:'./servercc.component.html'
+})
+export class ServerCCComponent{
+    
+}
+
+
+// NOTES:
+/**!SECTION
+ * Created a serverCC folder
+ * Created 2 files ts & html
+ * added the code required
+ * added the selector in app.component html
+ * declated the component in the app.module ts
+ * 
+ * all these are created manually to understand the flow.
+ * 
+ * Angualr has CLI command which does all this
+ * `ng g c component-name`
+ */
