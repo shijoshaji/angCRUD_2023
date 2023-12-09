3a6c8a82050daaba28dd93ec77c0922c7a226e52
Shijo Shaji <jojovms@gmail.com>
2023-12-09 19:31:39 +0530

    shopping feature

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,6 @@
+export class Ingredient{
+
+    constructor(public name:string, public qty:number, public qtyType:string){
+        // similar to recipe model, but more shorter way
+        }
+}
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.scss']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[]= [
+    new Ingredient('wheat',2,'Kg'),
+    new Ingredient('Apple',1,'Kg'),
+    new Ingredient('Tomato',500,'g')
+  ]
 
   constructor() { }
 
